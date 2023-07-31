475c2eb6d87cc0225b2263bad097cf377bf35ee4
Nicole-Rene Newcomb <112290345+NicoleReneNewcomb@users.noreply.github.com>
2023-07-30 21:27:25 -0700

    Reversed Token Assignment & App Functioning in Slack
    Reversed token assignment for bot token and app token. Basic functionality now works in slack workspace.

--- nrn_helper_bot.py
+++ nrn_helper_bot.py
@@ -1,7 +1,8 @@
-# Nicole-Rene's Helper Bot Slack App
-# Created by: Nicole-Rene Newcomb
-# Newest Version Date: 07/30/2023
-# Description: This Slack App Bot acts as a helper and assistant.
+""" Nicole-Rene's Helper Bot Slack App
+    Created by: Nicole-Rene Newcomb
+    Newest Version Date: 07/30/2023
+    Description: This Slack App Bot acts as a helper and assistant.
+"""
 
 import logging
 import os
@@ -20,23 +21,26 @@ logging.error(' - Error - ')
 # Setting up environmental variables
 # Using os.path to create relative link to .env instead of absolute - more portable
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
-SLACK_BOT_TOKEN = os.environ["NRN_Helper_Bot_Socket_Mode_Token"]
-SLACK_APP_TOKEN = os.environ["NRN_Helper_Bot_User_OAth_Token"]
+SLACK_BOT_TOKEN = os.environ["NRN_Helper_Bot_User_OAth_Token"]
+SLACK_APP_TOKEN = os.environ["NRN_Helper_Bot_Socket_Mode_Token"]
 
 # Create instance of slack app using bot socket token
 nrn_helper_bot = App(token=SLACK_BOT_TOKEN)
 
 # Create event handlers
-@app.event("app_mention")
+@nrn_helper_bot.event("app_mention")
 def mention_handler(body, context, payload, options, say, event):
+    """function to handle app_mention event"""
     say("Hello there! How can I help you?")
 
-@app.event("message")
+
+@nrn_helper_bot.event("message")
 def message_handler(body, context, payload, options, say, event):
+    """function to handle message event"""
     pass
 
 # Designating the main function
 if __name__ == "__main__":
     # Starting handlers
-    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+    handler = SocketModeHandler(nrn_helper_bot, SLACK_APP_TOKEN)
     handler.start()
